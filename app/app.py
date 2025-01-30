a6fcc3290918e19e9fd1a7640ffa1042927bed81
Romuloro <rrorodriguesoliveira@gmail.com>
2025-01-30 14:05:26 -0300

    refactor: put chat in other module and remove warnings

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ import streamlit as st
 
 from services import text, vectorstore, chain, image
 from infrastructure import prompt as ppt
+from infrastructure import chat
 
 base_dir = './data'
 dir_path_pdf = os.path.join(base_dir, "pdf")
@@ -22,28 +23,12 @@ MODEL = 'llama3.2-vision'
 IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif", ".bmp"}
 PDF_EXTENSION = ".pdf"
 
-def chat_stream(question,chain_, image_path=None):
-    # Prepare os dados de entrada para o chain_
-    inputs = {
-        "question": question,
-        "image_paths": image_path,  # Se necessário, passe o caminho das imagens aqui
-    }
 
-    response = f'{chain_.invoke(inputs)}'
-        
-    # Enviando a resposta caractere por caractere, como no exemplo original
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.02)
-
-
-def save_feedback(index):
-    st.session_state.history[index]["feedback"] = st.session_state[f"feedback_{index}"]
 
 def main():
         
-    st.set_page_config(page_title='ChatGpt Elis', page_icon=':books:')
-    st.header('Olá Elisangela. Converse com seus arquivos da pós')
+    st.set_page_config(page_title='Multimodal RAG', page_icon="💻")
+    st.header('Olá usuário, seja bem-vindo(a)(e)!!')
     #user_question = st.text_input('Faça uma pergunta para mim!')
 
     if "history" not in st.session_state:
@@ -61,62 +46,71 @@ def main():
                     "thumbs",
                     key=f"feedback_{i}",
                     disabled=feedback is not None,
-                    on_change=save_feedback,
+                    on_change=chat.save_feedback,
                     args=[i],
                 )
     
     
     with st.sidebar:
-    
-        st.subheader('Seus arquivos PDF', divider=True)
-        #precisa colcoar accept_multiple_files = TRue para ele carregar vários arquivos. Caso contrário só carrega um arquivo
-        uploaded_files = st.file_uploader("Envie seus arquivos (PDFs ou Imagens)", 
-                                    type=["pdf", "png", "jpg", "jpeg", "gif", "bmp"],
-                                    accept_multiple_files=True)
-        
-        pdf_files = []
-        image_files = []
+        st.title(":books: Documentos")
+        with st.container(border=True):
+            st.subheader('Seus arquivos PDF', divider=True)
+            #precisa colcoar accept_multiple_files = TRue para ele carregar vários arquivos. Caso contrário só carrega um arquivo
+            uploaded_files = st.file_uploader("Envie seus arquivos (PDFs)", 
+                                        type=["pdf"],
+                                        accept_multiple_files=True)
+            
+            pdf_files = []
+            image_files = []
 
-        if uploaded_files:
-            for uploaded_file in uploaded_files:
-                file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+            if uploaded_files:
+                for uploaded_file in uploaded_files:
+                    file_extension = os.path.splitext(uploaded_file.name)[1].lower()
 
-                # Determinar o diretório de destino com base na extensão
-                if file_extension == PDF_EXTENSION:
-                    save_path = os.path.join(dir_path_pdf, uploaded_file.name)
-                    pdf_files.append(save_path)
-                elif file_extension in IMAGE_EXTENSIONS:
-                    save_path = os.path.join(dir_path_image, uploaded_file.name)
-                    image_files.append(save_path)
-                else:
-                    st.warning(f"Extensão não suportada: {uploaded_file.name}")
-                    continue
+                    # Determinar o diretório de destino com base na extensão
+                    if file_extension == PDF_EXTENSION:
+                        save_path = os.path.join(dir_path_pdf, uploaded_file.name)
+                        pdf_files.append(save_path)
+                    elif file_extension in IMAGE_EXTENSIONS:
+                        save_path = os.path.join(dir_path_image, uploaded_file.name)
+                        image_files.append(save_path)
+                    else:
+                        st.warning(f"Extensão não suportada: {uploaded_file.name}")
+                        continue
 
-                # Se o arquivo já existir, apenas armazenamos o caminho
-                if os.path.exists(save_path):
-                    st.warning(f"Arquivo já existe: {uploaded_file.name}. Apenas registrando o caminho.")
-                else:
+                    # Se o arquivo já existir, apenas armazenamos o caminho
+                    #if os.path.exists(save_path):
+                        #st.warning(f"Arquivo já existe: {uploaded_file.name}. Apenas registrando o caminho.")
+                    #else:
                     # Salvar o arquivo no diretório apropriado
                     with open(save_path, "wb") as f:
                         f.write(uploaded_file.getbuffer())
-                    st.success(f"Arquivo salvo: {uploaded_file.name}")
+                    #st.success(f"Arquivo salvo: {uploaded_file.name}", icon="✅")
 
-            st.write("Todos os arquivos foram processados!")
+                st.success("Todos os arquivos foram processados!", icon="✅")
 
-        if st.button('Processar'):
+            if st.button("Processar PDF's"):
 
-            data_pdf = text.create_text_chunks(pdf_files)
-            data = data_pdf #+ data_image
+                data_pdf = text.create_text_chunks(pdf_files)
+                data = data_pdf #+ data_image
 
-            vectorstore_ = vectorstore.create_vectorstore(data, MODEL)
+                vectorstore_ = vectorstore.create_vectorstore(data, MODEL)
 
-            retriever = vectorstore_.as_retriever()
-            model = ChatOllama(model=MODEL, temperature=0)
-            parser = StrOutputParser()
+                retriever = vectorstore_.as_retriever()
+                model = ChatOllama(model=MODEL, temperature=0)
+                parser = StrOutputParser()
 
-            prompt_ = ppt.create_prompt()
+                prompt_ = ppt.create_prompt()
 
-            st.session_state.conversation = chain.create_chain(retriever, prompt_, model, parser)
+                st.session_state.conversation = chain.create_chain(retriever, prompt_, model, parser)
+
+
+        with st.container(border=True):
+            st.subheader('Seus arquivos de Imagens **EM IMPLEMENTAÇÃO**', divider=True, )
+            uploaded_files_image = st.file_uploader("Envie seus arquivos (Imagens)", 
+                                        type=["png", "jpg", "jpeg", "gif", "bmp"],
+                                        accept_multiple_files=True)
+            st.button('Processar Imagens')
     
     if prompt := st.chat_input("Say something"):
         with st.chat_message("user"):
@@ -125,7 +119,7 @@ def main():
         
         with st.chat_message("assistant"):
             with st.status("Pensando...", expanded=False) as status:
-                response = st.write_stream(chat_stream(prompt, st.session_state.conversation, image_files))
+                response = st.write_stream(chat.chat_stream(prompt, st.session_state.conversation, image_files))
             
             status.update(label="Concluído!", state="complete")
             st.session_state.history.append({"role": "assistant", "content": response})
@@ -133,7 +127,7 @@ def main():
             st.feedback(
                 "thumbs",
                 key=f"feedback_{len(st.session_state.history)}",
-                on_change=save_feedback,
+                on_change=chat.save_feedback,
                 args=[len(st.session_state.history)],
             )
 
